add024d81e14769f731501f4e9e71d2556398cab
Ivan Shanin <ivanxshanin@gmail.com>
2023-09-18 21:51:41 +0300

    add error boundary

--- src/index.js
+++ src/index.js
@@ -8,10 +8,47 @@ import App from "./components/app/app";
 // styles
 import "./index.css";
 
+
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      hasError: false 
+    };
+  };
+
+  static getDerivedStateFromError(error) {
+    return {hasError: true};
+  };
+
+  componentDidCatch(error, info) {
+    console.log("error:", error, info);
+  };
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <section>
+          <h1>Что-то пошло не так :(</h1>
+          <p>
+            В приложении произошла ошибка. Пожалуйста, перезагрузите страницу.
+          </p>
+        </section>
+      );
+    }
+    return this.props.children;
+  };
+};
+
+
+
 ReactDOM.render(
   (
     <React.StrictMode>
-      <App />
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>      
     </React.StrictMode>
   ),
   document.querySelector("#root")
