10f6b7ff9e322da1163f704c9e889d9ea68ef69c
Ivan Shanin <ivanxshanin@gmail.com>
2023-09-18 20:39:22 +0300

    create basic layout structure

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,17 +1,27 @@
+// components
+import AppHeader from "../app-header/app-header";
+import BurgerIngredients from "../burger-ingredients/burger-ingredients";
+import BurgerConstructor from "../burger-constructor/burger-constructor";
+
+// styles
 import styles from "./app.module.css";
+
+// data
 import { data } from "../../utils/data";
 
-function App() {
+
+
+export default function App() {
   return (
     <div className={styles.app}>
-      <pre style={{
-      	margin: "auto",
-      	fontSize: "1.5rem"
-      }}>
-      	Измените src/components/app/app.jsx и сохраните для обновления.
-      </pre>
+      <AppHeader />
+      <h1 className={styles.heading}>
+        Соберите бургер
+      </h1>
+      <main className={styles.main}>
+        <BurgerIngredients data={data} />
+        <BurgerConstructor data={data} />
+      </main>
     </div>
   );
-}
-
-export default App;
+};
