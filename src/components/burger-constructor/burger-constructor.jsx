7398ff1c81462358dcab4ceb6b5821bf6a4eecb4
Ivan Shanin <ivanxshanin@gmail.com>
2023-09-18 20:44:47 +0300

    create constructor section

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -1,6 +1,95 @@
+// libraries
+import React from "react";
+import PropTypes from "prop-types";
+
+// components
+import { TopRow } from "./row/row";
+import { BottomRow } from "./row/row";
+import { MiddleRow } from "./row/row";
+import { Button } from "@ya.praktikum/react-developer-burger-ui-components"
+import { CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components"
+
+// styles
 import styles from "./burger-constructor.module.css";
-import { data } from "../../utils/data";
 
-function BurgerConstructor() {};
+// utils
+import { ingredientPropType } from "../../utils/prop-types";
 
-export default BurgerConstructor;
+
+
+function getRandomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+};
+
+function getRandomElements(array, number) {
+  let result = [];
+  for (let i = 0; i < number; i++) {
+    result.push(getRandomElement(array))
+  }
+  return result;
+};
+
+
+
+export default function BurgerConstructor({ data }) {
+
+  const MemoizedIcon = React.memo(CurrencyIcon);
+
+  const [chosenBun, setChosenBun] = React.useState(
+    getRandomElement(
+      data.filter(
+        ingredient => ingredient.type === "bun"
+      )
+    )
+  );
+
+  const [chosenIngredients, setChosenIngredients] = React.useState(
+    getRandomElements(
+      data.filter(
+        ingredient => ingredient.type !== "bun"
+      ),
+      7
+    )
+  );
+
+  const [totalPrice, setTotalPrice] = React.useState(
+    chosenIngredients.reduce(
+      (cum, item) => cum + item.price, 
+      0
+    ) + chosenBun.price * 2
+  );
+
+  return (
+    <section className={styles.constructor}>
+      <ul className={styles.content}>
+        <TopRow bun={chosenBun}/>
+        <ul className={styles.contentScrollable}>
+          {
+            chosenIngredients.map(
+              (ingredient, index) => {
+                return <MiddleRow ingredient={ingredient} key={index} />
+              }
+            )
+          }
+        </ul>
+        <BottomRow bun={chosenBun}/>
+      </ul>
+      <div className={styles.summary}>
+        <p className={styles.price}>
+          {totalPrice} <MemoizedIcon type="primary" />
+        </p>        
+        <Button htmlType="button" type="primary" size="large">
+          Оформить заказ
+        </Button>        
+      </div>
+    </section>
+  );
+};
+
+BurgerConstructor.propTypes = PropTypes.exact(
+  {
+    data: PropTypes.arrayOf(
+      ingredientPropType.isRequired      
+    ).isRequired
+  }
+).isRequired;
---
+++ src/components/burger-constructor/row/row.jsx
@@ -0,0 +1,77 @@
+// libraries
+import PropTypes from "prop-types";
+
+// components
+import { DragIcon } from "@ya.praktikum/react-developer-burger-ui-components"
+import { ConstructorElement } from "@ya.praktikum/react-developer-burger-ui-components"
+
+// styles
+import styles from "./row.module.css";
+
+// utils
+import { ingredientPropType } from "../../../utils/prop-types";
+
+
+
+export function TopRow({ bun }) {
+  return (
+    <li className={styles.lockedRow}>
+      <ConstructorElement
+        type="top"
+        isLocked={true}
+        text={`${bun.name} (верх)`}
+        price={bun.price}
+        thumbnail={bun.image_large}
+      />
+    </li>
+  );
+};
+
+TopRow.propTypes = PropTypes.exact(
+  {
+    bun: ingredientPropType.isRequired
+  }
+).isRequired;
+
+
+
+export function BottomRow({ bun }) {
+  return (
+    <li className={styles.lockedRow}>
+      <ConstructorElement
+        type="bottom"
+        isLocked={true}
+        text={`${bun.name} (низ)`}
+        price={bun.price}
+        thumbnail={bun.image_large}
+      />
+    </li>
+  );
+};
+
+BottomRow.propTypes = PropTypes.exact(
+  {
+    bun: ingredientPropType.isRequired
+  }
+).isRequired;
+
+
+
+export function MiddleRow({ ingredient }) {
+  return (
+    <li className={styles.freeRow}>
+      <DragIcon type="primary" />  
+      <ConstructorElement
+        text={ingredient.name}
+        price={ingredient.price}
+        thumbnail={ingredient.image_large}
+      />
+    </li>  
+  );
+};
+
+MiddleRow.propTypes = PropTypes.exact(
+  {
+    ingredient: ingredientPropType.isRequired
+  }
+).isRequired;
