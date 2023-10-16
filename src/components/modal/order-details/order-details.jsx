442744854ece3e784a4dc3ffef3ce007f3991c73
Ivan Shanin <ivanxshanin@gmail.com>
2023-10-16 02:03:15 +0200

    add loader to order popup

--- src/components/modal/order-details/order-details.jsx
+++ src/components/modal/order-details/order-details.jsx
@@ -1,4 +1,5 @@
 // libraries
+import React from "react";
 import { useSelector } from "react-redux";
 
 // styles
@@ -6,40 +7,60 @@ import styles from "./order-details.module.css";
 
 // images
 import done from "../../../images/done.svg";
-
+import failed from "../../../images/failed.svg"
+import pending from "../../../images/pending.svg";
 
 
 function OrderDetails() {
 
-  const { placedOrder } = useSelector(state => state.burgerConstructor);
+  const { errorRequestingOrder, pendingRequestingOrder, placedOrder } = useSelector(state => state.burgerConstructor);
 
+  const [status, setStatus] = React.useState("");
+  const [action, setAction] = React.useState("");
+  const [suggestion, setSuggestion] = React.useState("");
+  const [iconSrc, setIconSrc] = React.useState("");
+
+  React.useEffect(
+    () => {
+      if (errorRequestingOrder) {
+        setStatus("не удалось создать заказ");
+        setAction("Что-то пошло не так");
+        setSuggestion("Лучше всего будет, если вы напишете в поддержку");
+        setIconSrc(failed);
+      } else if (pendingRequestingOrder) {
+        setStatus("создаём заказ");
+        setAction("Скоро начнём готовить заказ");
+        setSuggestion("Обычно это занимает совсем немного времени");
+        setIconSrc(pending);
+      } else {
+        setStatus("идентификатор заказа");
+        setAction("Ваш заказ начали готовить");
+        setSuggestion("Дождитесь готовности на орбитальной станции");
+        setIconSrc(done);
+      };
+    },
+    [errorRequestingOrder, pendingRequestingOrder]
+  );
+  
   return (
     <div className={styles.container}>
       <h3 className={styles.id}>
-        {placedOrder.order.number}
+        {placedOrder && placedOrder.order.number}
       </h3>
       <p className={styles.description}>
-        идентификатор заказа
+        {status}
       </p>
       <img 
-        src={done}
-        className={styles.done}
         alt="иконка статуса заказа: заказ успешно принят"
+        className={styles.done}
+        src={iconSrc}
       />
       <div className={styles.textArea}>
         <p className={styles.text}>
-          {
-            placedOrder.success 
-            ? "Ваш заказ начали готовить" 
-            : "Что-то пошло не так :("
-          }
+          {action}
         </p>      
         <p className={`${styles.text} ${styles.inactive}`}>
-          {
-            placedOrder.success 
-            ? "Дождитесь готовности на орбитальной станции" 
-            : "Лучше всего будет, если вы напишете в поддержку"
-          }    
+          {suggestion}    
         </p>      
       </div>
     </div>
