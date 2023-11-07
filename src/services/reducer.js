d3e54fd9eb0d764230e7295228b0ec1e2e18c6fa
Ivan Shanin <ivanxshanin@gmail.com>
2023-11-07 22:38:49 +0100

    create user slice thumbnail

--- src/services/reducer.js
+++ src/services/reducer.js
@@ -5,11 +5,13 @@ import { combineReducers } from "redux";
 import { modalSlice } from "./modal-slice";
 import { burgerIngredientsSlice } from "./burger-ingredients-slice";
 import { burgerConstructorSlice } from "./burger-constructor-slice";
+import { userSlice } from "./user-slice";
 
 
 
 export const rootReducer = combineReducers(
   {
+    user: userSlice.reducer,
     modal: modalSlice.reducer,
     burgerIngredients: burgerIngredientsSlice.reducer,
     burgerConstructor: burgerConstructorSlice.reducer
---
+++ src/services/user-slice.js
@@ -0,0 +1,182 @@
+// libraries
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { postRequest } from "../api/request";
+
+
+
+// import {setUser, setAuthChecked} from "./user";
+// import {api} from "../utils/api";
+
+// export const getUser = () => {
+//     return (dispatch) => {
+//         return api.getUser().then((res) => {
+//             dispatch(setUser(res.user));
+//         });
+//     };
+// };
+
+// export const checkUserAuth = () => {
+//     return (dispatch) => {
+//         if (localStorage.getItem("accessToken")) {
+//             dispatch(getUser())
+//                 .catch(() => {
+//                     localStorage.removeItem("accessToken");
+//                     localStorage.removeItem("refreshToken");
+//                     dispatch(setUser(null));
+//                 })
+//                 .finally(() => dispatch(setAuthChecked(true)));
+//         } else {
+//             dispatch(setAuthChecked(true));
+//         }
+//     };
+// };
+
+
+
+export const loginUser = createAsyncThunk(
+  "user/loginUser",
+  ({ email, password }) => {
+    return postRequest(
+      "auth/login/", { email, password }
+    ).then(
+      response => {
+        localStorage.setItem("accessToken", response.accessToken);
+        localStorage.setItem("refreshToken", response.refreshToken);
+        return response.user;
+      }
+    );
+  }
+);
+
+export const createUser = createAsyncThunk(
+  "user/createUser",
+  ({ email, password, name }) => {
+    return postRequest(
+      "auth/register/", { email, password, name }
+    ).then(
+      response => {
+        localStorage.setItem("accessToken", response.accessToken);
+        localStorage.setItem("refreshToken", response.refreshToken);
+        return response.user;
+      }
+    );
+  }
+);
+
+export const logoutUser = createAsyncThunk(
+  "user/logoutUser",
+  () => {
+    return postRequest(
+      "auth/logout/", {
+        token: localStorage.getItem("refreshToken"),
+      }
+    ).then(
+      response => {
+        localStorage.removeItem("accessToken");
+        localStorage.removeItem("refreshToken");
+      }
+    );
+  }
+);
+
+export const updateAccessToken = createAsyncThunk(
+  "user/updateAccessToken",
+  () => {
+    return postRequest(
+      "auth/token/", {
+        token: localStorage.getItem("refreshToken"),
+      }      
+    ).then(
+      response => {
+        localStorage.setItem("accessToken", response.accessToken);
+        localStorage.setItem("refreshToken", response.refreshToken);
+      }      
+    )
+  }
+);
+
+
+
+export const userSlice = createSlice(
+  {
+    name: "user",
+    initialState: {
+      currentUser: null,
+      // isAuthorized: false,
+      errorAuthorizingUser: false,
+      pendingAuthorizingUser: false,
+      errorDeauthorizingUser: false,
+      pendingDeauthorizingUser: false,
+    },
+    reducers: {
+      resetUser: state => {
+        state.currentUser = null;
+        // state.isAuthorized = false;
+      },
+    },
+    extraReducers: builder => {
+      builder.addCase(
+        loginUser.pending,
+        state => {
+          state.pendingAuthorizingUser = true;
+        }
+      ).addCase(
+        loginUser.rejected,
+        (state, action) => {
+          state.errorAuthorizingUser = true;
+          state.pendingAuthorizingUser = false;
+          console.error(action.payload);
+        }
+      ).addCase(
+        loginUser.fulfilled,
+        (state, action) => {
+          state.errorAuthorizingUser = false;
+          state.pendingAuthorizingUser = false;
+          state.currentUser = action.payload;
+        }
+      ).addCase(
+        createUser.pending,
+        state => {
+          state.pendingAuthorizingUser = true;
+        }
+      ).addCase(
+        createUser.rejected,
+        (state, action) => {
+          state.errorAuthorizingUser = true;
+          state.pendingAuthorizingUser = false;
+          console.error(action.payload);
+        }
+      ).addCase(
+        createUser.fulfilled,
+        (state, action) => {
+          state.errorAuthorizingUser = false;
+          state.pendingAuthorizingUser = false;
+          state.currentUser = action.payload;
+        }
+      ).addCase(
+        logoutUser.pending,
+        state => {
+          state.pendingDeauthorizingUser = true;
+        }
+      ).addCase(
+        logoutUser.rejected,
+        (state, action) => {
+          state.errorDeauthorizingUser = true;
+          state.pendingDeauthorizingUser = false;
+          console.error(action.payload);
+        }
+      ).addCase(
+        logoutUser.fulfilled,
+        state => {
+          state.errorDeauthorizingUser = false;
+          state.pendingDeauthorizingUser = false;
+          state.currentUser = null;
+        }
+      ).addDefaultCase(
+        state => state
+      );
+    }
+  }
+);
+
+export const { setAuthChecked, setUser } = userSlice.actions;
