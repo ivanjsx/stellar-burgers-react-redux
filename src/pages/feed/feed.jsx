e16163398dd2fb3f70d0557d5d139ccd5f3a8734
Ivan Shanin <ivanxshanin@gmail.com>
2023-11-09 01:38:43 +0100

    create order & feed pages thumbnails to test routing

---
+++ src/pages/feed/feed.jsx
@@ -0,0 +1,13 @@
+// components
+import { ErrorPage } from "../../pages";
+
+// styles 
+import styles from "./feed.module.css";
+
+
+
+function FeedPage() {
+  return <ErrorPage title={"Эта страница пока в разработке"} showTips={true} />                
+};
+
+export default FeedPage;
