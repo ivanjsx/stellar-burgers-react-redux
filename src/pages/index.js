7ca84d1975adf7c410990daa00f48da1aa0c4ce6
Ivan Shanin <ivanxshanin@gmail.com>
2023-11-09 01:48:40 +0100

    create history page thumbnail in profile route

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,10 @@
 import HomePage from "./home/home";
+import FeedPage from "./feed/feed";
 import ErrorPage from "./error/error";
 import LoginPage from "./login/login";
+import OrderPage from "./order/order";
+import HistoryPage from "./history/history";
+import LoadingPage from "./loading/loading";
 import ProfilePage from "./profile/profile";
 import RegisterPage from "./register/register";
 import IngredientPage from "./ingredient/ingredient";
@@ -9,8 +13,12 @@ import ForgotPasswordPage from "./forgot-password/forgot-password";
 
 export {
   HomePage,
+  FeedPage,
   ErrorPage,
   LoginPage,
+  OrderPage,
+  HistoryPage,
+  LoadingPage,
   ProfilePage,
   RegisterPage,
   IngredientPage,
