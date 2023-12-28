d4b5dc98586e0f4c77f44c6d15da3a29e0056b6b
Ivan Shanin <ivanxshanin@gmail.com>
2023-12-28 13:05:51 +0700

    convert burger-ingredients slice to typescript

--- src/services/burger-ingredients/burger-ingredients-slice.ts
+++ src/services/burger-ingredients/burger-ingredients-slice.ts
@@ -7,23 +7,34 @@ import { BURGER_INGREDIENTS_STATE_NAME } from "../../utils/constants";
 // actions
 import { requestAvailableStock } from "./burger-ingredients-thunks";
 
+// types 
+import { IngredientType } from "../../utils/types";
+
+
+
+type InitialStateType = {
+  availableStock: Map<string, IngredientType>,
+  errorRequestingIngredients: boolean,
+  pendingRequestingIngredients: boolean,
+}
+
+const initialState: InitialStateType = {
+  availableStock: new Map(),
+  errorRequestingIngredients: false,
+  pendingRequestingIngredients: false,
+}
+
 
 
 const burgerIngredientsSlice = createSlice(
   {
     name: BURGER_INGREDIENTS_STATE_NAME,
-    
-    initialState: {
-      availableStock: new Map(),
-      errorRequestingIngredients: false,
-      pendingRequestingIngredients: false,
-    },
-    
+    initialState,
     reducers: {},
     
-    extraReducers: builder => {
+    extraReducers: (builder) => {
       builder.addCase(
-        requestAvailableStock.pending, state => {
+        requestAvailableStock.pending, (state) => {
           state.pendingRequestingIngredients = true;
         }
       ).addCase(
@@ -39,7 +50,7 @@ const burgerIngredientsSlice = createSlice(
           state.availableStock = action.payload;
         }
       ).addDefaultCase(
-        state => state
+        (state) => state
       );
     },
   }
