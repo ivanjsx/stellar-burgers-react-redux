2533c1d7046d7f00d6c35ce1e67d0d55e392709f
Ivan Shanin <ivanxshanin@gmail.com>
2023-12-28 12:49:05 +0700

    convert order-feed slice to typescript

--- src/services/order-feed/order-feed-slice.ts
+++ src/services/order-feed/order-feed-slice.ts
@@ -1,5 +1,5 @@
 // libraries
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 // constants 
 import { 
@@ -9,22 +9,39 @@ import {
   ORDER_FEED_STATE_NAME,
 } from "../../utils/constants";
 
+// types 
+import { FeedMessageType, OrderType } from "../../utils/types";
+
+
+
+type InitialStateType = {
+  todaysTotal: number,
+  allTimeTotal: number,
+  orders: Map<number, OrderType>,
+  connectionError: boolean,
+  connectionStatus: typeof ONLINE 
+                  | typeof OFFLINE 
+                  | typeof CONNECTING 
+                  | typeof ORDER_FEED_STATE_NAME
+}
+
+const initialState: InitialStateType = {
+  todaysTotal: 0,
+  allTimeTotal: 0,
+  orders: new Map(),
+  connectionError: false,
+  connectionStatus: OFFLINE,
+}
+
 
 
 const orderFeedSlice = createSlice(
   {
     name: ORDER_FEED_STATE_NAME,
-    
-    initialState: {
-      todaysTotal: 0,
-      allTimeTotal: 0,
-      orders: new Map(),
-      connectionError: false,
-      connectionStatus: OFFLINE,
-    },
+    initialState,
     
     reducers: {
-      connect: (state, action) => {
+      connect: (state, action: PayloadAction<string>) => {
         state.connectionStatus = CONNECTING;
       },
       disconnect: state => {
@@ -41,7 +58,7 @@ const orderFeedSlice = createSlice(
       onError: state => {
         state.connectionError = true;
       },
-      onMessage: (state, action) => {
+      onMessage: (state, action: PayloadAction<FeedMessageType>) => {
         state.allTimeTotal = action.payload.total;
         state.todaysTotal = action.payload.totalToday;
         state.orders = new Map(
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,3 +1,14 @@
 export type IngredientType = {
   id: string,
 }
+
+export type OrderType = {
+  id: string,
+  number: number,
+}
+
+export type FeedMessageType = {
+  total: number,
+  totalToday: number,
+  orders: Array<OrderType>
+}
