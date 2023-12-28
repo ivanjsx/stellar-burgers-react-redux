5daf4cdfb309e2b0cc923ac1156ba29c168a7582
Ivan Shanin <ivanxshanin@gmail.com>
2023-12-28 11:49:21 +0700

    indicate correctly typed middlewares in store

--- src/services/store.ts
+++ src/services/store.ts
@@ -46,9 +46,15 @@ const store = configureStore(
   { 
     reducer: rootReducer,
     devTools: process.env.NODE_ENV !== "production",
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware(
-      { serializableCheck: false }
-    ).concat(logger, thunk, orderFeedMiddleware)
+    middleware: (getDefaultMiddleware) => {
+      return getDefaultMiddleware(
+        { serializableCheck: false }
+      ).concat(
+        logger, 
+        thunk, 
+        orderFeedMiddleware      
+      )
+    }
   }
 );
 
